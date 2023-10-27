334ae7af98a6b3afba67ccdb5910037257050388
Ilham Adiputra <ilhamadiputra4@gmail.com>
2023-10-27 13:51:10 +0700

    chore(packages/react): remove unnecessary files

---
+++ apps/web/components/form.tsx
@@ -0,0 +1,17 @@
+import { Form } from "../lezzform/_generated/form";
+
+export const ProductForm = () => {
+  return (
+    <div
+      style={{
+        padding: "1rem",
+        borderRadius: "0.5rem",
+        backgroundColor: "white",
+        border: "1px solid grey",
+        maxWidth: "fit-content",
+      }}
+    >
+      <Form />
+    </div>
+  );
+};
