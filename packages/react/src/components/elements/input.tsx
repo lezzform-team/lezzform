c1e9fc19f3ea94ab13b0dd709fdcfd64f86c96ee
Ilham Adiputra <ilhamadiputra4@gmail.com>
2023-12-21 17:53:03 +0700

    chore(packages/react): show label sign isRequired

--- packages/react/src/components/elements/input.tsx
+++ packages/react/src/components/elements/input.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { Input as ShadInput } from "../ui/input";
+import { FormLabel } from "../shared";
 
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   id?: string;
   label?: string;
   error?: string;
+  isRequired?: boolean;
 }
 
-export const Input = ({ id, label, error, ...props }: Props) => {
+export const Input = ({ id, label, error, isRequired, ...props }: Props) => {
   return (
     <div className="flex flex-col gap-2">
-      <label htmlFor={id} className="text-sm font-medium leading-none">
+      <FormLabel htmlFor={id} isRequired={isRequired}>
         {label}
-      </label>
-      <ShadInput id={id} {...props} />
+      </FormLabel>
+      <ShadInput id={id} required={isRequired} {...props} />
       {error && (
         <small className="text-xs font-medium leading-none text-red-500">
           {error}
