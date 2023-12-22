86fdd890b0051faa3a56ba41240ca4d068398371
Ilham Adiputra <ilhamadiputra4@gmail.com>
2023-12-22 16:06:13 +0700

    refactor(packages/react): move from tanstack form to rhf

--- packages/react/src/components/elements/input.tsx
+++ packages/react/src/components/elements/input.tsx
@@ -1,26 +1,11 @@
 import React from "react";
 import { Input as ShadInput } from "../ui/input";
-import { FormLabel } from "../shared";
 
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
-  id?: string;
-  label?: string;
-  error?: string;
   isRequired?: boolean;
+  label?: string;
 }
 
-export const Input = ({ id, label, error, isRequired, ...props }: Props) => {
-  return (
-    <div className="flex flex-col gap-2">
-      <FormLabel htmlFor={id} isRequired={isRequired}>
-        {label}
-      </FormLabel>
-      <ShadInput id={id} required={isRequired} {...props} />
-      {error && (
-        <small className="text-xs font-medium leading-none text-red-500">
-          {error}
-        </small>
-      )}
-    </div>
-  );
+export const Input = ({ isRequired, ...props }: Props) => {
+  return <ShadInput required={isRequired} {...props} />;
 };
--- packages/react/src/components/elements/textArea.tsx
+++ packages/react/src/components/elements/textArea.tsx
@@ -1,27 +1,11 @@
 import React from "react";
 import { Textarea } from "../ui/textarea";
-import { FormLabel } from "../shared";
 
 interface Props extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
-  id?: string;
-  label?: string;
-  error?: string;
   isRequired?: boolean;
+  label?: string;
 }
 
-export const TextArea = ({ id, label, error, isRequired, ...props }: Props) => {
-  return (
-    <div className="flex flex-col gap-2">
-      <FormLabel htmlFor={id} isRequired={isRequired}>
-        {label}
-      </FormLabel>
-      <Textarea id={id} required={isRequired} {...props} />
-
-      {error && (
-        <small className="text-xs font-medium leading-none text-red-500">
-          {error}
-        </small>
-      )}
-    </div>
-  );
+export const TextArea = ({ isRequired, ...props }: Props) => {
+  return <Textarea required={isRequired} {...props} />;
 };
