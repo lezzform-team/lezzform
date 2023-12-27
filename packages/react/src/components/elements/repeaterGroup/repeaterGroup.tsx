b17f5f93436238e328bfb83f1566466d5dfa2561
Ilham Adiputra <ilhamadiputra4@gmail.com>
2023-12-27 21:30:43 +0700

    feat(packages/react): add repeatedGroup remove item

--- packages/react/src/components/elements/repeaterGroup/repeaterGroup.tsx
+++ packages/react/src/components/elements/repeaterGroup/repeaterGroup.tsx
@@ -3,20 +3,36 @@ import { DetailedHTMLProps, HTMLAttributes } from "react";
 import { RepeaterGroupWrapper, RepeaterGroupWrapperProps } from "./wrapper";
 import { FieldValues } from "react-hook-form";
 
+interface RepeaterGroupProps
+  extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+  onDelete?: () => unknown;
+  readonly?: boolean;
+}
+
 export function RepeaterGroup({
   children,
   className,
+  onDelete,
+  readonly,
   ...props
-}: DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>) {
+}: RepeaterGroupProps) {
   return (
     <div
       className={cn(
-        "w-full flex flex-col gap-2 p-2 border border-lfui-muted rounded-md",
+        "w-full flex flex-col gap-2 p-2 border border-lfui-muted rounded-md relative",
         className,
       )}
       {...props}
     >
       {children}
+      {!readonly && (
+        <button
+          onClick={onDelete}
+          className="px-2 py-1 text-xs rounded-sm bg-red-100 text-red-500 absolute top-1 right-1"
+        >
+          Delete
+        </button>
+      )}
     </div>
   );
 }
