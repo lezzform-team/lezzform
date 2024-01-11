5db517eb0da9d63693b0d9acaee2012fd9b48884
Ilham Adiputra <ilhamadiputra4@gmail.com>
2024-01-11 07:04:34 +0700

    feat(packages/rnative): ready for library

---
+++ packages/rnative/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @lezzform/react-native
+
+## 0.0.2
+
+### Patch Changes
+
+- try publish
--- packages/rnative/src/components/elements/input.tsx
+++ packages/rnative/src/components/elements/input.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import {Input as UIInput} from '../ui/input';
+import {StyleSheet} from 'react-native';
+
+const style = StyleSheet.create({input: {color: 'red'}});
 
 const Input = () => {
-  return <UIInput />;
+  return <UIInput style={style.input} />;
 };
 Input.displayName = 'Input';
 
