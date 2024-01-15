915486157d84e8b81f35bc11569b363f5831485c
Ilham Adiputra <ilhamadiputra4@gmail.com>
2024-01-15 16:27:34 +0700

    chore(packages/rnative): basic component input improve

--- packages/rnative/src/components/elements/input.tsx
+++ packages/rnative/src/components/elements/input.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
-import {Input as UIInput} from '../ui/input';
+import React, {useMemo} from 'react';
+import {InputProps, Input as UIInput} from '../ui/input';
 import {StyleSheet} from 'react-native';
 
-const style = StyleSheet.create({input: {color: 'red'}});
+interface Props extends InputProps {}
 
-const Input = () => {
-  return <UIInput style={style.input} />;
+const Input = ({...props}: Props) => {
+  const style = useMemo(() => {
+    return StyleSheet.create({
+      Input: {
+        color: 'black',
+      },
+    });
+  }, []);
+
+  return <UIInput style={style.Input} {...props} />;
 };
 Input.displayName = 'Input';
 
