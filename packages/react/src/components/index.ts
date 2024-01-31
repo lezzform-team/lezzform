f83829fe4886a57ae16ae68b619a5682fbad1173
Ilham Adiputra <ilhamadiputra4@gmail.com>
2024-01-31 22:15:55 +0700

    feat(packages/react): flash message component support

--- packages/react/src/components/index.ts
+++ packages/react/src/components/index.ts
@@ -1,3 +1,4 @@
 export * from "./elements";
 export * from "./form";
 export * from "./ui/form";
+export * from "./shared";
--- packages/react/src/components/shared/form-message.tsx
+++
@@ -1,9 +0,0 @@
-import { HTMLProps } from "react";
-
-export function FormMessage({ children }: HTMLProps<HTMLElement>) {
-  return (
-    <small className="text-xs font-medium leading-none text-red-500">
-      {children}
-    </small>
-  );
-}
