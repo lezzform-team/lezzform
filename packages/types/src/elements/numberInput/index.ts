2617920faf37f738cd69045ab00402372069e1d3
Ilham Adiputra <ilhamadiputra4@gmail.com>
2024-02-05 22:40:10 +0700

    feat(packages/types): support for number input variant type

--- packages/types/src/elements/numberInput/index.ts
+++ packages/types/src/elements/numberInput/index.ts
@@ -5,10 +5,32 @@ import {
 } from "../..";
 import { IntegerRule } from "../../rules";
 
-export type NumberInputType = GenericLezzformElement<GenericFieldAttributes> & {
+export type NumberInputType = GenericLezzformElement<
+  GenericFieldAttributes & NumberInputAttributesType
+> & {
   type: "NumberInput";
   category: LezzformElementCategory.Field;
   rule: NumberInputRuleType;
+};
+
+export type NumberInputAttributesType = {
+  variant: NumberInputAttributesVariant;
+};
+
+export type NumberInputAttributesVariant =
+  | NumberInputDefaultVariantType
+  | NumberInputStepperVariantType;
+
+export type NumberInputDefaultVariantType = {
+  type: "Default";
+};
+
+export type NumberInputStepperVariantType = {
+  type: "Stepper";
+  step: {
+    increment: number;
+    decrement: number;
+  };
 };
 
 export type NumberInputRuleType = IntegerRule;
